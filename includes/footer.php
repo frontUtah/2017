8bf4d07eebf6b1e348bdf40e4e11a45f07cb8039
Wade Shearer <wade.shearer@gmail.com>
2017-05-07 18:04:06 -0600

    Add header and footer.

---
+++ includes/footer.php
@@ -0,0 +1,49 @@
+
+	<footer>
+		<nav class="social-nav">
+			<ul>
+				<li><a class="twitter" href="https://twitter.com/front">Twitter</a></li>
+				<li><a class="linkedin" href="https://www.linkedin.com/company-beta/4873375/?pathWildcard=4873375">LinkedIn</a></li>
+				<li><a class="instagram" href="https://www.instagram.com/frontutah/">Instagram</a></li>
+			</ul>
+		</nav>
+		<nav class="main-footer-nav">
+			<ul>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">Terms &amp; Conditions</a></li>
+				<li><a href="#">Code of Conduct</a></li>
+				<li><a href="#">Speaker Guide</a></li>
+			</ul>
+		</nav>
+		<p>Join us at the</p>
+		<h3 class="logo">Front</h3>
+		<p>&copy; Front, LLC. All rights reserved.</p>
+	</footer>
+	<script src="scripts/jquery-3.2.1.min.js"></script>
+	<script src="scripts/jquery.magnific-popup.js"></script>
+	<script src="scripts/ui.js"></script>
+	<script>
+		$(function() {
+			var imageNumber = Math.floor( Math.random() * 7 ) + 1;
+			$( '.hero' ).addClass( 'image-' + imageNumber );
+			
+			
+			
+	      $(document).ready(function() {
+	        $('.recap-video').magnificPopup({
+	          disableOn: 700,
+	          type: 'iframe',
+	          mainClass: 'mfp-fade',
+	          removalDelay: 160,
+	          preloader: false,
+
+	          fixedContentPos: false
+	        });
+	      });
+
+			
+			
+		});
+	</script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $title; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="/styles/style.css">
+	<link rel="stylesheet" href="/styles/magnific-popup.css">
+</head>
+<body>
+
+<div class="headers">
+	<header class="primary-header">
+		<nav class="primary-nav">
+			<ul>
+				<!--Mobile Order: Actual Order. Desktop Order: Class Names. Bless you Flexbox-->
+				<li class="order-1"><a href="/" class="home active">Home</a></li>
+				<li class="order-2"><a href="/conference" class="case-study">Case Study Conference</a></li>
+				<li class="order-3"><a href="/bootcamp" class="bootcamp">Product Bootcamp</a></li>
+				<li class="order-4"><a href="/onsite" class="custom-training">Custom Onsite Training</a></li>
+				<li class="order-5"><a href="https://medium.com/front-conference" class="magazine">Magazine</a></li>
+			</ul>
+		</nav>
+		<nav class="social-nav">
+			<ul>
+				<li><a class="twitter" href="https://twitter.com/front">Twitter</a></li>
+				<li><a class="linkedin" href="https://www.linkedin.com/company/front-utah">LinkedIn</a></li>
+				<li><a class="instagram" href="https://www.instagram.com/frontutah/">Instagram</a></li>
+			</ul>
+		</nav>
+	</header>
+</div>
