a435ed76bdc297beec149634ef122a48f02df18e
Wade Shearer <wade.shearer@gmail.com>
2017-05-07 22:42:03 -0600

    Real text, images, and links.

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ require('includes/header.php');
 					<img src="images/conf-scene001.jpg" alt="">
 				</div>
 				<p class="description">A two-day, in-depth series  of from-the-trenches case studies by innovative user experience designers and product managers.</p>
-				<p><a href="#" class="button">Learn More</a></p>
+				<p><a href="/conference" class="button">Learn More</a></p>
 			</li>
 			<li>
 				<h3>Product Bootcamp</h3>
@@ -30,7 +30,7 @@ require('includes/header.php');
 					<img src="images/conf-scene002.jpg" alt="">
 				</div>
 				<p class="description">A two-day, hands-on training bootcamp to learn how to create amazing products and work like the teams.</p>
-				<p><a href="#" class="button">Learn More</a></p>
+				<p><a href="/bootcamp" class="button">Learn More</a></p>
 			</li>
 			<li>
 				<h3>Custom Onsite Training</h3>
@@ -38,51 +38,51 @@ require('includes/header.php');
 				<div class="thumnail">
 					<img src="images/conf-scene003.jpg" alt="">
 				</div>
-				<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-				<p><a href="#" class="button">Learn More</a></p>
+				<p class="description">Let us to bring Front to you. Tell us about your organization's needs and we'll build a custom training solution to meet them.</p>
+				<p><a href="/onsite" class="button">Learn More</a></p>
 			</li>
 		</ul>
 	</section>
 	<section class="about">
 		<h2>Building amazing product teams</h2>
-		<p>Front is the product conference for UX designers and product managers. It's a practitioner’s conference. Attendees come for an education, real world, from-the-trenches case studies, and hands-on training from their peers. It's four total days of learning and growth, providing answers to deep organizational questions and insights into how to validate and solve the right problems in the right way.</p>
+		<p>Creating amazing products requires more than brilliant ideas, meticulously crafted interactions, perfect pixels, and cutting-edge technology—it requires a fanatical focus on users and a commitment to producing something that is both useful and desirable. It requires cross-functional teams that listen, iterate, and learn.<br /><br />Front is the product conference for UX designers and product managers. It's a practitioner’s conference. Attendees come for an education, real world, from-the-trenches case studies, and hands-on training from their peers. It's four days of learning and growth, providing answers to deep organizational questions and insights into how to validate and solve the right problems in the right way.</p>
 	</section>
 	<section class="personal-stories">
 		<h2>Personal stories from innovative teams</h2>
 		<ul class="story-cards">
 			<li>
-				<div href="https://vimeo.com/167029277" class="card-image recap-video"><img src="images/front-speaker-sian-townsend.jpg" alt=""></div>
+				<a href="https://vimeo.com/167029277"><div href="https://vimeo.com/167029277" class="card-image recap-video"><img src="images/front-speaker-sian-townsend.jpg" alt=""></div></a>
 				<h3>Sian Townsend</h3>
 				<p>Jobs to be Done: from Doubter to Believer</p>
 			</li>
 			<li>
-				<div href="https://vimeo.com/167025212" class="card-image recap-video"><img src="images/front-speaker-sam-vanessa.jpg" alt=""></div>
+				<a href="https://vimeo.com/167025212"><div href="https://vimeo.com/167025212" class="card-image recap-video"><img src="images/front-speaker-sam-vanessa.jpg" alt=""></div></a>
 				<h3>Sam Goertler &amp; Vanessa Koch</h3>
 				<p>Last things first: A redesign strategy that&hellip;</p>
 			</li>
 			<li>
-				<div href="https://vimeo.com/167736107" class="card-image recap-video"><img src="images/front-speaker-tracey-varnell.jpg" alt=""></div>
+				<a href="https://vimeo.com/167736107"><div href="https://vimeo.com/167736107" class="card-image recap-video"><img src="images/front-speaker-tracey-varnell.jpg" alt=""></div></a>
 				<h3>Tracey Varnell</h3>
 				<p>Collaborative techniques for product leaders</p>
 			</li>
 			<li>
-				<div href="https://vimeo.com/166885565" class="card-image recap-video"><img src="images/front-speaker-bob-baxley.jpg" alt=""></div>
+				<a href="https://vimeo.com/166885565"><div href="https://vimeo.com/166885565" class="card-image recap-video"><img src="images/front-speaker-bob-baxley.jpg" alt=""></div></a>
 				<h3>Bob Baxley</h3>
 				<p>Designing the machine that designs the designs</p>
 			</li>
 			<li>
-				<div href="https://vimeo.com/167004431" class="card-image recap-video"><img src="images/front-speaker-keenan-cummings.jpg" alt=""></div>
+				<a href="https://vimeo.com/167004431"><div href="https://vimeo.com/167004431" class="card-image recap-video"><img src="images/front-speaker-keenan-cummings.jpg" alt=""></div></a>
 				<h3>Keenan Cummings</h3>
 				<p>Product vision and storytelling</p>
 			</li>
 			<li>
-				<div href="https://vimeo.com/167006595" class="card-image recap-video"><img src="images/front-speaker-kyle-jacobsen.jpg" alt=""></div>
+				<a href="https://vimeo.com/167006595"><div href="https://vimeo.com/167006595" class="card-image recap-video"><img src="images/front-speaker-kyle-jacobsen.jpg" alt=""></div></a>
 				<h3>Kyle Jacobson</h3>
 				<p>Where did the Duke Boys go?</p>
 			</li>
 		</ul>
 		<p>
-			<a href="#" class="button">Watch More</a>
+			<a href="https://vimeo.com/frontutah" class="button">Watch More</a>
 		</p>
 	</section>
 	<section class="attendee-stats">
