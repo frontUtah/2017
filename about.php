00876305eb5d87aa2ff14deaa8a0b97f04ffe810
Wade Shearer <wade.shearer@gmail.com>
2017-05-11 12:05:37 -0600

    Updated stats to 2017 numbers.

--- about.php
+++ about.php
@@ -13,16 +13,39 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 		</section>
 		<section class="body short">
 			<h2>Inspiration and training</h2>
-			<p>Front is brought to you by Andrew Branch, Ben Peck, and Wade Shearer, experienced designers and product managers, and cofounders of Product Hive (originally called the Product Design Association), now bosting over 2,500 members. After years of hosting meetups, lunches, and workshops, we learned that there is a hunger for training and advanced topics. People want to hear from their peers. They want to see what other teams are working on and struggling with and how they have overcome those challenges. They’re looking for more than a motivational speaker or a party—they want insights into how they can make a big impact at their companies and in the world. They want more than trends and theory. They want answers to deep organizational questions and how to validate that they were solving the right problems in the right way.</p>
+			<p>Front is brought to you by Andrew Branch, Ben Peck, and Wade Shearer, experienced designers and product managers, 
+				and cofounders of Product Hive (originally called the Product Design Association), now bosting over 2,500 members. 
+				After years of hosting meetups, lunches, and workshops, we learned that there is a hunger for training and advanced 
+				topics. People want to hear from their peers. They want to see what other teams are working on and struggling with 
+				and how they have overcome those challenges. They’re looking for more than a motivational speaker or a party—they 
+				want insights into how they can make a big impact at their companies and in the world. They want more than trends 
+				and theory. They want answers to deep organizational questions and how to validate that they were solving the right 
+				problems in the right way.</p>
 		</section>
 		<section class="photo-collage">
 			<img src="/images/leadership.jpg" alt="">
 		</section>
 		<section class="body">
 			<h4>Brought to you by Ben Peck, Andrew Branch, and Wade Shearer</h4>
-			<p>We were bringing people together, but the impact was small. We continued to meet amazing teams doing amazing work, but there was so little interaction and visibility between them. Again and again we would hear about how easy it is to get lost within the walls of an organization. It was time for something big. It was time to bring the industry together for something new. In 2016, we sold out with over 600 attendees from 5 countries and 28 states across the US.</p>
-			<p>This is a different kind of conference. We are hyper-focused on one thing: extraordinary content. We don’t have a call for speakers. There’s no alcohol. There’s no after-party. Each contributor is sought out for the specific work they are doing and how their team is innovating. Requirements to present are for deep case studies, showcasing the good and the bad. The primary story and experience is learning and growth.</p>
-			<p>Front is named for the Wasatch Front, the metropolitan region in North-central Utah, that stretches along the Wasatch Mountain Range. The Wasatch Range stretches from the Idaho border, south through central Utah and is the western edge of the greater Rocky Mountains. For us, Front is also the fore-front of technology and innovation and product experience—the part you touch, hear, or interact with.
+			<p>We were bringing people together, but the impact was small. We continued 
+				to meet amazing teams doing amazing work, but there was so little 
+				interaction and visibility between them. Again and again we would hear 
+				about how easy it is to get lost within the walls of an organization. 
+				It was time for something big. It was time to bring the industry together 
+				for something new. In 2017, we sold out with 650 attendees from 
+				4 countries and 29 states across the US.</p>
+			<p>This is a different kind of conference. We are hyper-focused on one thing: 
+				extraordinary content. We don’t have a call for speakers. There’s no 
+				alcohol. There’s no after-party. Each contributor is sought out for the 
+				specific work they are doing and how their team is innovating. 
+				Requirements to present are for deep case studies, showcasing the good 
+				and the bad. The primary story and experience is learning and growth.</p>
+			<p>Front is named for the Wasatch Front, the metropolitan region in 
+				North-central Utah, that stretches along the Wasatch Mountain Range. 
+				The Wasatch Range stretches from the Idaho border, south through 
+				central Utah and is the western edge of the greater Rocky Mountains. 
+				For us, Front is also the fore-front of technology and innovation and 
+				product experience—the part you touch, hear, or interact with.
 			<p>Join us at the Front to share, learn, and be inspired to create amazing products.</p>
 		</section>
 	</main>
