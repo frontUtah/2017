1d61a7754064da3f9adf4fc6168ac6344a7d9713
Wade Shearer <wade.shearer@gmail.com>
2017-05-13 00:05:00 -0600

    Added Google analytics to the footer

--- includes/footer.php
+++ includes/footer.php
@@ -19,6 +19,15 @@
 		<h3 class="logo">Front</h3>
 		<p>&copy; Front, LLC. All rights reserved.</p>
 	</footer>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-60121892-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 	<script src="/scripts/jquery-3.2.1.min.js"></script>
 	<script src="/scripts/jquery.magnific-popup.js"></script>
 	<script src="/scripts/ui.js"></script>
