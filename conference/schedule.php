80a12f6b5c750046170139fadf2e47f0ed6919ae
wadeshearer <me@wadeshearer.com>
2017-09-03 22:01:59 -0600

    New schedule

--- conference/schedule.php
+++ conference/schedule.php
@@ -14,7 +14,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 		<section class="schedule-breakdown">
 			<h2>Schedule</h2>
 			<div class="schedule-day">
-				<h3>Thursday, 4 January 2018</h3>
+				<h3>Thursday, 31 May 2018</h3>
 				<ol class="timeline">
 					<li class="timeslot one-hour">
 						<dl>
@@ -24,137 +24,224 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 							</dd>
 						</dl>
 					</li>
-					<li class="timeslot four-hours">
+					<li class="timeslot one-hour">
 						<dl>
-							<dt class="time">8:30 AM</dt>
+							<dt class="time">9:00 AM</dt>
+							<dd class="food">
+								<p>Opening remarks</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">9:10 AM</dt>
 							<dd class="ux">
-								<h4>UX</h4>
-								<p>Basics</p>
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
 							</dd>
-							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Basics</p>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">9:50 AM</dt>
+							<dd class="ux">
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
 							</dd>
-							<dd class="research">
-								<h4>Research</h4>
-								<p>Basics</p>
-							</dd>
-							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Advanced</p>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">10:30 AM</dt>
+							<dd class="ux">
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
 							</dd>
 						</dl>
 					</li>
 					<li class="timeslot one-hour">
 						<dl>
-							<dt class="time">12:30 PM</dt>
+							<dt class="time">11:10 AM</dt>
 							<dd class="food">
-								<p>Hot Lunch</p>
+								<p>Break (drinks and snacks provided)</p>
 							</dd>
 						</dl>
 					</li>
 					<li class="timeslot two-hours">
 						<dl>
-							<dt class="time">1:30 PM</dt>
+							<dt class="time">11:50 AM</dt>
 							<dd class="ux">
-								<h4>UX</h4>
-								<p>Basics</p>
-							</dd>
-							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Basics</p>
-							</dd>
-							<dd class="research">
-								<h4>Research</h4>
-								<p>Basics</p>
-							</dd>
-							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Basics</p>
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
 							</dd>
 						</dl>
 					</li>
 					<li class="timeslot two-hours">
 						<dl>
-							<dt class="time">3:30 PM</dt>
+							<dt class="time">12:30 PM</dt>
 							<dd class="ux">
-								<h4>UX</h4>
-								<p>Advanced</p>
+								<h4>Q&A panel with morning speakers</h4>
+								<p>TBD</p>
 							</dd>
-							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Advanced</p>
+						</dl>
+					</li>
+					<li class="timeslot one-hour">
+						<dl>
+							<dt class="time">1:00 PM</dt>
+							<dd class="food">
+								<p>Lunch break</p>
 							</dd>
-							<dd class="research">
-								<h4>Research</h4>
-								<p>Advanced</p>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">2:30 PM</dt>
+							<dd class="ux">
+								<h4>Case Study Team Talk</h4>
+								<p>TBD</p>
 							</dd>
-							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Basics</p>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">3:10 PM</dt>
+							<dd class="ux">
+								<h4>Case Study Team Talk</h4>
+								<p>TBD</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">3:50 PM</dt>
+							<dd class="ux">
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot one-hour">
+						<dl>
+							<dt class="time">9:00 AM</dt>
+							<dd class="food">
+								<p>Day 1 wrap-up</p>
 							</dd>
 						</dl>
 					</li>
 				</ol>
 			</div>
 			<div class="schedule-day">
-				<h3>Friday, 5 January 2018</h3>
+				<h3>Friday, 1 June 2018</h3>
 				<ol class="timeline">
 					<li class="timeslot one-hour">
 						<dl>
 							<dt class="time">7:30 AM</dt>
 							<dd class="food">
-								<p>Hot Breakfast and Regestration</p>
-							</dd>
-						</dl>
-					</li>
-					<li class="timeslot four-hours">
-						<dl>
-							<dt class="time">8:30 AM</dt>
-							<dd class="ux">
-								<h4>UX</h4>
-								<p>Advanced</p>
-							</dd>
-							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Basic</p>
-							</dd>
-							<dd class="research">
-								<h4>Research</h4>
-								<p>Basics</p>
-							</dd>
-							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Advanced</p>
+								<p>Hot Breakfast</p>
 							</dd>
 						</dl>
 					</li>
 					<li class="timeslot one-hour">
 						<dl>
-							<dt class="time">12:30 PM</dt>
+							<dt class="time">9:00 AM</dt>
 							<dd class="food">
-								<p>Hot Lunch</p>
+								<p>Day 2 kick-off</p>
 							</dd>
 						</dl>
 					</li>
-					<li class="timeslot four-hours">
+					<li class="timeslot two-hours">
 						<dl>
-							<dt class="time">1:30 PM</dt>
+							<dt class="time">9:10 AM</dt>
 							<dd class="ux">
-								<h4>UX</h4>
-								<p>Advanced</p>
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
 							</dd>
-							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Advanced</p>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">9:50 AM</dt>
+							<dd class="ux">
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
 							</dd>
-							<dd class="research">
-								<h4>Research</h4>
-								<p>Advanced</p>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">10:30 AM</dt>
+							<dd class="ux">
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
 							</dd>
-							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Advanced</p>
+						</dl>
+					</li>
+					<li class="timeslot one-hour">
+						<dl>
+							<dt class="time">11:10 AM</dt>
+							<dd class="food">
+								<p>Break (drinks and snacks provided)</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">11:50 AM</dt>
+							<dd class="ux">
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">12:30 PM</dt>
+							<dd class="ux">
+								<h4>Q&A panel with morning speakers</h4>
+								<p>TBD</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot one-hour">
+						<dl>
+							<dt class="time">1:00 PM</dt>
+							<dd class="food">
+								<p>Lunch break</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">2:30 PM</dt>
+							<dd class="ux">
+								<h4>Case Study Team Talk</h4>
+								<p>TBD</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">3:10 PM</dt>
+							<dd class="ux">
+								<h4>Case Study Team Talk</h4>
+								<p>TBD</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot two-hours">
+						<dl>
+							<dt class="time">3:50 PM</dt>
+							<dd class="ux">
+								<h4>Case Study Talk</h4>
+								<p>TBD</p>
+							</dd>
+						</dl>
+					</li>
+					<li class="timeslot one-hour">
+						<dl>
+							<dt class="time">4:30 PM</dt>
+							<dd class="food">
+								<p>Closing remarks</p>
 							</dd>
 						</dl>
 					</li>
