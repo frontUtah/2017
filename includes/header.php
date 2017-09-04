c3c2869f9cc874aa5bf7b09ed7ede0f496df9a3b
wadeshearer <me@wadeshearer.com>
2017-09-03 22:00:49 -0600

    Hid talks and speakers pages

--- includes/header.php
+++ includes/header.php
@@ -76,9 +76,9 @@ foreach($sorted_global_nav as $key => $nav) {
 switch($uri_parts[0]) {
 	case 'conference':
 
-		$conference_nav[] = array('link' => 'talks', 'text' => 'Talks', 'class' => '');
+		//$conference_nav[] = array('link' => 'talks', 'text' => 'Talks', 'class' => '');
 		$conference_nav[] = array('link' => 'schedule', 'text' => 'Schedule', 'class' => '');
-		$conference_nav[] = array('link' => 'speakers', 'text' => 'Speakers', 'class' => '');
+		//$conference_nav[] = array('link' => 'speakers', 'text' => 'Speakers', 'class' => '');
 		$conference_nav[] = array('link' => 'venue', 'text' => 'Venue', 'class' => '');
 		$conference_nav[] = array('link' => 'faqs', 'text' => 'FAQs', 'class' => '');
 		$conference_nav[] = array('link' => 'whyattend', 'text' => 'Why attend', 'class' => '');
