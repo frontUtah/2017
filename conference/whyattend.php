fa548c5e7185c49bb8b7c0300822143294cf8cdc
Ben Peck <ben@benpeck.com>
2017-09-11 13:37:30 -0600

    Why Attend / Schedule
    Updated the copy and images on Why Attend and the header image on schedule.

--- conference/whyattend.php
+++ conference/whyattend.php
@@ -13,40 +13,45 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 		</section>
 		<section class="reasons-why">
 			<h2>Why Attend</h2>
-			<p class="about-reasons-why">You’ve sat through the keynotes, listened to the podcasts, and read the books. Now what? Where do you begin? You’re motivated and inspired, but what if your organization isn’t or what if you’re building a new team? How do you begin? Front Park City is a two-day bootcamp, specifically designed as an intimate, intensive, one-of-a-kind, hands-on UX and Product Management training experience where you will  experience and practice the techniques and methods that will accelerate the efficiency and accuracy and your organization’s product discovery and delivery. It also happens to be held in the majestic mountains of Utah, blanketed with the greatest snow on earth.
+			<p class="about-reasons-why">Front was built around the idea of sharing ideas and helping one another learn from other people’s experiences. Attending the Front Conference you’ll get to meet and learn from people that have been building digital experiences for years. You’ll learn how others are solving problems, celebrating successes, and building cultures of creativity and innovation.
+			<br>
+			<br>
 
-It’s time to demand more than a workshop, bolted onto a conference as a supplemental day of content. Come, join us at the Front, where you’ll work side-by-side with some of the industry’s best and brightest. Design a custom training program to jump-starting your career or take it to the next level.
+
+We believe product management and product design play a central role in the success of a product. We believe we need better, smarter people to lead product. We believe in human relationships and learning from each other through examples. We believe in kindness, generousity, transparency and a willingness to share.  
+
 </p>
+		
 			<ul class="reasons-why-list">
 				<li>
-					<img src="/images/conf-scene004.jpg" alt="">
-					<h4>Multi-track hands-on workshops</h4>
-					<p>Design your custom training program, choosing from basic, intermediate, and advanced courses.</p>
+					<img src="/images/conf-scene-front-01.jpg" alt="">
+					<h4>Team Structure and Growth </h4>
+					<p>Bring your team and meet others working in your industry and have opportunities to help each other grow.</p>
 				</li>
 				<li>
-					<img src="/images/conf-scene005.jpg" alt="">
-					<h4>Greatest snow on earth</h4>
-					<p>Don’t just get away from your desk, get out of the city. Stay through the weekend riding on pure Utah powder.</p>
+					<img src="/images/conf-scene-front-02.jpg" alt="">
+					<h4>Learn from leaders</h4>
+					<p>Listen to leaders in our industry that are making the biggest impact and working with high performing teams.</p>
 				</li>
 				<li>
-					<img src="/images/conf-scene006.jpg" alt="">
-					<h4>Two intense days of learning</h4>
-					<p>Our bootcamp consists of six, deep workshops where you’ll learn and practice methods and techniques crucial to building game-changing products.</p>
+					<img src="/images/conf-scene-front-03.jpg" alt="">
+					<h4>See new perspectives and be inspired</h4>
+					<p>See the problem from a different perspective and learn from those that might not be close to the issues you’re dealing with.</p>
 				</li>
 				<li>
-					<img src="/images/conf-scene007.jpg" alt="">
-					<h4>Practicing instructors</h4>
-					<p>The best teachers are those that lead by example. Most Front instructors are employed with leading technology companies.</p>
+					<img src="/images/conf-scene-front-04.jpg" alt="">
+					<h4>Personal Improvement</h4>
+					<p>Learn ways that you can advance in your career through soft and hard skills that will help you compete in this competitive tech industry.</p>
 				</li>
 				<li>
-					<img src="/images/conf-scene008.jpg" alt="">
-					<h4>Gourmet meals</h4>
-					<p>No boxed lunch lunches with cold-cut sandwiches or continental breakfasts. We treat you right with hot, gourmet meals.</p>
+					<img src="/images/conf-scene-front-05.jpg" alt="">
+					<h4>Product leadership skills </h4>
+					<p>Learn not only how to be a part of a team but how to lead. You’ll learn how great product teams function and how you can too. </p>
 				</li>
 				<li>
-					<img src="/images/conf-scene009.jpg" alt="">
-					<h4>Networking and fun</h4>
-					<p>Historic Park City, skiing, food, and friends—you’ll leave after two days with a wealth of memories and new, professional connections.</p>
+					<img src="/images/conf-scene-front-06.jpg" alt="">
+					<h4>Dedicated deep dive </h4>
+					<p>Focused time away from your desk allows you get out of your own head and look at things with fresh eyes.</p>
 				</li>
 			</ul>
 		</section>
