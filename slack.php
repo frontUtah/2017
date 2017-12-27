94cef97226292e6be7fe8444c0046e2cf9827ad9
wadeshearer <me@wadeshearer.com>
2017-12-27 09:57:56 -0700

    Added Slack vanity URL redirect

---
+++ slack.php
@@ -0,0 +1,5 @@
+<?php
+
+header('location: https://join.slack.com/t/frontutah/shared_invite/enQtMjkyNTY0NzgyNjYzLWFjMTM5MzM3ZTQ3YTEzODc5Mzk0NjNhMDI3NjliYWQ4YWFiZGYxMTI3NGMxOGViMGE1YTQ0ODU1OTQ4NGYwY2Y');	
+	
+?>
