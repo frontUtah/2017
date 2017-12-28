d09273e8a834e464f28cd16ab8cdf06d0a4286cd
wadeshearer <me@wadeshearer.com>
2017-12-28 11:03:31 -0700

    Filled out the schedule

--- bootcamp/schedule.php
+++ bootcamp/schedule.php
@@ -14,6 +14,31 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 		<section class="schedule-breakdown">
 			<h2>Schedule</h2>
 			<div class="schedule-day">
+				<ol class="timeline">
+					<li class="timeslot category">
+						<dl>
+							<dt class="time"></dt>
+							<dd class="ux">
+								<h4>UX</h4>
+								<p>Quinney Welcome Center Theater</p>
+							</dd>
+							<dd class="pm">
+								<h4>PM</h4>
+								<p>Quinney Welcome Center, Conference</p>
+							</dd>
+							<dd class="research">
+								<h4>Research</h4>
+								<p>Legacy Center, Lower</p>
+							</dd>
+							<dd class="leadership">
+								<h4>Leadership</h4>
+								<p>Legacy Center, Upper</p>
+							</dd>
+						</dl>
+					</li>
+				</ol>
+			</div>
+			<div class="schedule-day">
 				<h3>Thursday, 4 January 2018</h3>
 				<ol class="timeline">
 					<li class="timeslot one-hour">
@@ -28,20 +53,28 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 						<dl>
 							<dt class="time">8:30 AM</dt>
 							<dd class="ux">
-								<h4>UX</h4>
-								<p>Basics</p>
+								<a href="/bootcamp/instructor/sarahdoody">
+									<h4>Sarah Doody</h4>
+									<p>Product storyboarding</p>
+								</a>
 							</dd>
 							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Basics</p>
+								<a href="/bootcamp/instructor/virgiliapruthi">
+									<h4>Virgilia Pruthi</h4>
+									<p>Building and communicating roadmaps</p>
+								</a>
 							</dd>
 							<dd class="research">
-								<h4>Research</h4>
-								<p>Basics</p>
+								<a href="/bootcamp/instructor/jayfichialos">
+									<h4>Jay Fichialos</h4>
+									<p>Product discovery</p>
+								</a>
 							</dd>
 							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/richardbanfield">
+									<h4>Richard Banfield</h4>
+									<p>Best practices of high performing teams</p>
+								</a>
 							</dd>
 						</dl>
 					</li>
@@ -57,20 +90,28 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 						<dl>
 							<dt class="time">1:30 PM</dt>
 							<dd class="ux">
-								<h4>UX</h4>
-								<p>Basics</p>
+								<a href="/bootcamp/instructor/jonathanshariat">
+									<h4>Jonathan S.</h4>
+									<p>Design ethics</p>
+								</a>
 							</dd>
 							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Basics</p>
+								<a href="/bootcamp/instructor/johncutler">
+									<h4>John Cutler</h4>
+									<p>Alignment through mapping</p>
+								</a>
 							</dd>
 							<dd class="research">
-								<h4>Research</h4>
-								<p>Basics</p>
+								<a href="/bootcamp/instructor/taylorpalmer">
+									<h4>Taylor Palmer</h4>
+									<p>Usability testing with prototypes</p>
+								</a>
 							</dd>
 							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Basics</p>
+								<a href="/bootcamp/instructor/bobbaxley">
+									<h4>Bob Baxley</h4>
+									<p>Design feedback</p>
+								</a>
 							</dd>
 						</dl>
 					</li>
@@ -78,20 +119,28 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 						<dl>
 							<dt class="time">3:45 PM</dt>
 							<dd class="ux">
-								<h4>UX</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/jeffcarter">
+									<h4>Jeff Carter</h4>
+									<p>Micro interactions</p>
+								</a>
 							</dd>
 							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/kylejacobsen">
+									<h4>Kyle & Josh</h4>
+									<p>Metrics</p>
+								</a>
 							</dd>
 							<dd class="research">
-								<h4>Research</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/amandasmith">
+									<h4>Amanda Smith</h4>
+									<p>Growing a UX department</p>
+								</a>
 							</dd>
 							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Basics</p>
+								<a href="/bootcamp/instructor/danlawyer">
+									<h4>Dan Lawyer</h4>
+									<p>Finding & building executive presence</p>
+								</a>
 							</dd>
 						</dl>
 					</li>
@@ -112,20 +161,28 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 						<dl>
 							<dt class="time">8:30 AM</dt>
 							<dd class="ux">
-								<h4>UX</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/laurengray">
+									<h4>Lauren Gray</h4>
+									<p>On-boarding</p>
+								</a>
 							</dd>
 							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Basic</p>
+								<a href="/bootcamp/instructor/elainechao">
+									<h4>Elaine Chao</h4>
+									<p>Better listening, better products</p>
+								</a>
 							</dd>
 							<dd class="research">
-								<h4>Research</h4>
-								<p>Basics</p>
+								<a href="/bootcamp/instructor/lorieich">
+									<h4>Lori Eich</h4>
+									<p>Early stage quant and qual validation</p>
+								</a>
 							</dd>
 							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/mariahhay">
+									<h4>Mariah Hay</h4>
+									<p>Product leadership fundamentals</p>
+								</a>
 							</dd>
 						</dl>
 					</li>
@@ -141,20 +198,28 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 						<dl>
 							<dt class="time">1:30 PM</dt>
 							<dd class="ux">
-								<h4>UX</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/chadheinrich">
+									<h4>Chad Heinrich</h4>
+									<p>Designing for growth, virality</p>
+								</a>
 							</dd>
 							<dd class="pm">
-								<h4>Product Management</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/natebarrett">
+									<h4>Nate Barrett</h4>
+									<p>Value proposition and vision</p>
+								</a>
 							</dd>
 							<dd class="research">
-								<h4>Research</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/andrewbranch">
+									<h4>Andrew Branch</h4>
+									<p>Lean user testing</p>
+								</a>
 							</dd>
 							<dd class="leadership">
-								<h4>Leadership</h4>
-								<p>Advanced</p>
+								<a href="/bootcamp/instructor/wadeshearer">
+									<h4>Wade Shearer</h4>
+									<p>Teaching your team to learn</p>
+								</a>
 							</dd>
 						</dl>
 					</li>
