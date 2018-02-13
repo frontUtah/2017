eed383a5db3ca1b1e1dccc8972388d045ab032e9
wadeshearer <me@wadeshearer.com>
2018-02-12 21:56:32 -0700

    Updated bootcamp registration (Eventbrite) URL

--- bootcamp/registration.php
+++ bootcamp/registration.php
@@ -107,7 +107,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 			</div>
 			-->
 			
-			<p><a target="_blank" href="http://frontparkcity2018.eventbrite.com" class="button">Register</a></p>
+			<p><a target="_blank" href="https://frontbootcampnov2018.eventbrite.com" class="button">Register</a></p>
 		</section>
 
 		<section class="accomodations">
