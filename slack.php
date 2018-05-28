b2a2739d485264cf73f98d3c3a7c1c6a811d46c6
wadeshearer <me@wadeshearer.com>
2018-05-28 08:58:50 -0600

    Updated Slack link

--- slack.php
+++ slack.php
@@ -1,5 +1,5 @@
 <?php
 
-header('location: https://join.slack.com/t/frontutah/shared_invite/enQtMjkyNTY0NzgyNjYzLWFjMTM5MzM3ZTQ3YTEzODc5Mzk0NjNhMDI3NjliYWQ4YWFiZGYxMTI3NGMxOGViMGE1YTQ0ODU1OTQ4NGYwY2Y');	
+header('location: https://join.slack.com/t/frontutah/shared_invite/enQtMzYzNzc0MDk5ODc0LTA1NWZjODdkYWU3ZDZlOWI4YzZkMjFkYTc4OGIzNDA3MzMyMTA2YWUyMmRhNDJkMzU5NmFmZTNkNDlkNmNlYWQ');	
 	
 ?>
