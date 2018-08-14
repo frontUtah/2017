a451be8652d692f9851ba6546a4a57c9d270cb3e
Ben Peck <ben@frontutah.com>
2018-08-14 16:51:45 -0400

    Fixed Danyel's name and link

--- workshops/schedule.php
+++ workshops/schedule.php
@@ -166,8 +166,8 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 								</a>
 							</dd>
 							<dd class="research course" onclick="">
-								<a href="https://www.frontutah.com/workshops/instructor/joelbeukelman">
-									<p><strong>Danyel Rio Printz</strong></p>
+								<a href="https://www.frontutah.com/workshops/instructor/danyelriospritz">
+									<p><strong>Danyel Rios Printz</strong></p>
 									<p>Uncovering insights while maintaining your sanity</p>
 								</a>
 							</dd>
