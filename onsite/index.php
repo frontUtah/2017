7400696cd83ae2d0f262055c996f41c5e52f3563
Ben Peck <ben@frontutah.com>
2018-10-16 09:18:10 -0400

    syncing where my I moved my Git files

--- onsite/index.php
+++ onsite/index.php
@@ -7,52 +7,89 @@ $title = 'Front - Custom onsite UX & Product Management training';
 require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 
 ?>
-	<main class="custom-training">
+	<style type="text/css">
+	.course-onsite{
+		display: flex; 
+		flex-direction: row;
+		text-align: left;
+	}
+	.course-onsite .course-onsite-description:first-child {
+		width: 60%;
+		margin-right: 75px; 
+	}
+	.course-onsite .button {
+		font-size: 18px;
+		padding: 10px 30px;
+		margin-top: 40px;
+	}
+	.course-onsite-description {
+		
+	}
+	.course-onsite-description h3 {
+		text-align: center;
+	}
+	.course-onsite-description h4 {
+		font-size: 25px;
+		font-weight: bold;
+		color: #FD7C00;
+	}
+	.course-onsite-info {
+		padding: 40px;
+	}
+	.course-onsite-info span {
+		padding-right: 30px;
+	}
+	.course-onsite-instructors {
+		display: flex; 
+		flex-direction: row;
+		padding-left: 0px; 
+	}
+	.course-onsite-instructors img{
+		width: 125px;
+		margin-right: 24px;
+	}
+	.course-onsite-instructors .course-onsite-instructors-info {
+		color: #FD7C00;
+	}
+	.course-onsite-instructors .course-onsite-instructors-info h5 {
+		font-size: 20px;
+		font-weight: bold;
+		color: #FD7C00;
+	}
+	.course-onsite-instructors .course-onsite-instructors-info p {
+		font-size: 14px;
+		font-weight: lighter;
+		margin: 10px 0px;
+	}
+</style>
+	<main class="custom-training secondary product-workshops-page workshops-courses">
 		<section class="hero">
 			<h1>Custom onsite training</h1>
 			<p>Let us to bring Front to you. Tell us about your organization's needs and we'll build a custom training solution to meet them.</p>
 		</section>
-		<section class="custom-training-about">
-			<h2>Learning to learn, in context</h2>
-			<p>Artifacts from your attempts to ship product likely cover the walls of your war rooms. You’re keenly aware of the competitive threats your feeling from the market. Your team’s strengths and weaknesses are pretty clear. The window to deliver value is likely closing in. Imagine an intensive, multi-day onsite training, customized specifically for your team, to build out a high-functioning, customer-left product organization.</p>
-		</section>
-		<section class="training-courses">
-			<h2>Design your perfect training program</h2>
-			<p>Whether you’re starting from ground zero or looking to fortify a team of seasoned professionals, we’ll work together with you to build a custom training solution that specifically delivers on your desired outcome. By bringing the courses to your team, you are able to control the pace, reach, and context, delivering much deeper and lasting results.</p>
-			<ul class="course-groups">
-				<li class="course-group ux">
-					<dl>
-						<dt>UX</dt>
-						<dd class="advanced">Advanced</dd>
-						<dd class="intermediate">Intermediate</dd>
-						<dd class="basic">Basic</dd>
-					</dl>
-				</li>
-				<li class="course-group pm">
-					<dl>
-						<dt>Product Management</dt>
-						<dd class="advanced">Advanced</dd>
-						<dd class="intermediate">Intermediate</dd>
-						<dd class="basic">Basic</dd>
-					</dl>
-				</li>
-				<li class="course-group research">
-					<dl>
-						<dt>Research</dt>
-						<dd class="advanced">Advanced</dd>
-						<dd class="intermediate">Intermediate</dd>
-						<dd class="basic">Basic</dd>
-					</dl>
-				</li>
-				<li class="course-group leadership">
-					<dl>
-						<dt>Leadership</dt>
-						<dd class="advanced">Advanced</dd>
-						<dd class="intermediate">Intermediate</dd>
-						<dd class="basic">Basic</dd>
-					</dl>
-				</li>
-			</ul>
+		<section class="courses">
+			<div class="course-calendar course-onsite">
+				<div class="course-onsite-description">
+					<h3>UX Courses</h3>
+					<div class="course-onsite-info">
+						<h4>Design Sprint</h4>
+						<p><span>Size: <strong>25</strong></span><span>Length: <strong>One Week</strong></span><span>Cost: <strong>$50,000</strong></span></p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+						<p><a href="#" class="button">Learn More</a></p>
+					</div>
+				</div>
+				<div class="course-onsite-description" style="width: 40%">
+					<h3>UX Instructors</h3>
+					<div class="course-onsite-info course-onsite-instructors">
+						<div><img src="https://www.frontutah.com/images/frontworkshops18/speaker_joelbeukelman.jpg"></div>
+						<div class="course-onsite-instructors-info">
+							<h5>Joel Beukelman</h5>
+							<p>Product Designer<br>
+							Google</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 
 		<section class="request-form">
