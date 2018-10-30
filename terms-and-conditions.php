a0c590fb75dd91f9edb232c82e9e1aa31b540619
Wade Shearer <me@wadeshearer.com>
2018-10-30 16:40:24 -0600

    Updated to include graduate programs to student discount

--- terms-and-conditions.php
+++ terms-and-conditions.php
@@ -14,7 +14,14 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 		<section class="body">
 			<h2>Terms and conditions</h2>
 			<h4>Terms of Service</h4>
-			<p>By purchasing a ticket to Front, you agree that you have read, and you agree to abide by the terms outlined here. Attendees must be 21 years or older. All sales are final. Tickets may not be sold. Tickets may be transferred to someone else until two weeks before the event. Sponsorship, press, or complimentary passes may not be transferred. Attendees are eligible for the student discount if they are enrolled full-time in a college or university undergraduate program and not working full-time.</p>
+			<p>By purchasing a ticket to Front, you agree that you have read, and 
+				you agree to abide by the terms outlined here. Attendees must be 21 
+				years or older. All sales are final. Tickets may not be sold. Tickets 
+				may be transferred to someone else until two weeks before the event. 
+				Sponsorship, press, or complimentary passes may not be transferred. 
+				Attendees are eligible for the student discount if they are enrolled 
+				full-time in a college or university undergraduate or graduate program and not 
+				working full-time.</p>
 
 			<h4>Privacy Policy</h4>
 			<p>We care about your privacy. Here’s what we collect about you and what we use it for. We collect anonymous information about visitors to our website as part of the ticket purchase process, using Eventbrite. We collect a small amount of personal information when you register for a ticket so that we can create badges, prepare attendee lists, order t-shirts, gauge geographical interest, and to communicate with you about the event. Besides anonymous information about companies and geographic representation that we provide sponsors, we will not sell or share your information with anyone.</p>
