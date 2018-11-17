ce096077fe2c26b2248f8e74a36b4908a58d471a
Ben Peck <ben@frontutah.com>
2018-11-17 01:54:35 -0500

    Updates to the onsite training

---
+++ onsite/consultant.php
@@ -0,0 +1,61 @@
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/init.php');
+
+$title = 'Front - Custom onsite UX & Product Management training';
+
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
+
+?>
+
+<style type="text/css">
+	.course-description h3:not(:first-child) {padding-top: 20px; border-top: 1px solid rgba(0,0,0,.09);}
+</style>
+
+<main class="secondary product-workshops-page">
+	<section class="hero">
+		<h1>Consultants</h1>
+	</section>
+	<section class="individual-instructor">
+		<h2 class="">Consultants</h2>
+		<div class="instructor-profile">
+			<div class="details">
+				<div class="instructor ux">
+					<div class="instructor-photo">
+						<img src="/images/frontworkshops18/speaker_benpeck.jpg" alt="">
+					</div>
+					<h4>Ben Peck</h4>
+					<p class="title">Cofounder<br>Front</p>
+				</div>
+				<nav class="social-nav">
+					<ul>
+						<li><a class="twitter" href="https://twitter.com/benpeck">Twitter</a></li><li><a class="linkedin" href="https://www.linkedin.com/in/bpeck/">LinkedIn</a></li><li><a class="instagram" href="https://www.instagram.com/benpeck/">Instagram</a></li>						
+					</ul>
+				</nav>
+			</div>
+			<div class="instructor-content">
+				<div class="course-description">
+					<h3>Design Critiques Done Right</h3><p>Showing our work at all different levels is a core part of being a designer. Running a critique with your team that’s effective and helpful all around for everyone isn’t an easy task when it comes to pushing everyone to be better but also being respectful. Come learn ways that you can improve these processes and make these gatherings enjoyable and exciting again. </p>
+					<p>Price: $7,000.00</p>
+					<h3>Revolution vs Evolution</h3><p>Showing our work at all different levels is a core part of being a designer. Running a critique with your team that’s effective and helpful all around for everyone isn’t an easy task when it comes to pushing everyone to be better but also being respectful. Come learn ways that you can improve these processes and make these gatherings enjoyable and exciting again. </p>
+					<p>Price: $7,000.00</p>
+				</div>
+				<div class="about-instructor">
+					<h3>About Ben</h3>
+					<p>Ben comes from over a decade of design experience working with software companies that have partnered with brands such as Nike, The North Face, Oakley, Under Armour, and Sonos. Ben is focused on designing software and products to significantly impact the user experience needs in both consumer and enterprise industries. Ben is also the cofounder of the Front Conference, a product design and management conference held in Utah which is on it 4rd year, as well as the Director of Product Hive with over 3,000+ product designers and product managers.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="join-us">
+		<h2>Join us at the Front!</h2>
+		<p>Reserve your seat now for the premier UX and Product Management training experience. Design your custom training program now. Whether you’re a beginner or seasoned professional, the Front UX & Product Management Workshop Series will take you and your team to the next level in product design and management.</p>
+		<p><a href="/workshops/registration" class="button">Register</a></p>
+	</section>
+</main>
+
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
+
+?>
