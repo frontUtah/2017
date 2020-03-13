bf17ae177ed4b22dd7c1f55197d13797891be970
Wade Shearer <me@wadeshearer.com>
2020-03-13 07:14:20 -0600

    Coronavirus notice

---
+++ coronavirus.php
@@ -0,0 +1,85 @@
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/init.php');
+
+$title = 'Speakers - Front UX & Product Management Case Study Conference, 28-29 May 2020';
+
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
+
+?>
+	<main class="article-page">
+		<section class="moreserious hero">
+			<h1>Notice regarding coronavirus (COVID-19)</h1>
+		</section>
+		<section class="body short">
+			<h2>Notice regarding coronavirus (COVID-19)</h2>
+			<p>The health and safety of our attendees, speakers, sponsors, and staff is our highest priority. After monitoring developments of the coronavirus pandemic and considering the latest recommendations and requests of major global health organizations and governments, we have decided to postpone the 2020 Front UX & Product Management Case Study Conference. </p>
+			<p>It is with sad hearts that delay this event. We have been so eager to gather with you again for the sixth year. After a year of research and planning, we have crafted a program of some of the best and brightest practitioners in the industry and set to be our most exciting and inspiring event yet. Postponing the event, however, is the right thing to do.</p>
+			<p>The good news is that Front isn’t being cancelled. Instead of two separate events year year, the Case Study Conference will be combined with our Workshop Series in November, for a single, large, multi-format event. We will also be expanding our usual schedule to include round tables, a welcome reception, yoga, a group meditation session, a service project, networking dinners, and an after-party.</p>
+			<p>The combined event will be 9-13 November 2020, with the workshops Tuesday and Wednesday and the talks Thursday and Friday. More details about the schedule will be available shortly on our website.</p>
+			<p>If you have already purchased tickets, you don’t need to do anything. Your tickets will automatically be transitioned to the new schedule. If you have not, we encourage you to register now and reserve your spot and take advantage of early bird pricing. The event will sell out and there are limited seats in the workshops. Tickets will be sold as a combination for the talks and workshops, or for just the talks alone.</p>
+			<p>We appreciate your flexibility and support during this time. We would also like to thank Pluralsight and all of our other wonderful sponsors for their support and collaboration in making these events a success. Our thoughts and prayers are with everyone affected by the coronavirus situation.</p>
+		</section>
+			
+		<section class="faqs">
+			<ul class="faq-categories">
+				<li class="faq-category">
+					<dl class="accordion">
+						<dt class="accordion-title question">Why are you postponing the event?</dt>
+						<dd class="accordion-content answer">The health and safety of our attendees, speakers, sponsors, and staff is our highest priority. Spending time together is a key value of our events, but limiting gatherings of groups is critical in the abatement of the coronavirus pandemic. Thus, we are postponing the conference to a later date when we will be able to safely gather.</dd>
+					</dl>
+					<dl class="accordion">
+						<dt class="accordion-title question">When will the Conference be held?</dt>
+						<dd class="accordion-content answer">The conference will be combined with our workshop series, for a single, larger, multi-format event, 9-13 November 2020. The workshops will be Tuesday and Wednesday and the talks Thursday and Friday.</dd>
+					</dl>
+					<dl class="accordion">
+						<dt class="accordion-title question">Did registration cancellations impact the decision?</dt>
+						<dd class="accordion-content answer">No. In fact, we only had 3 cancellations.</dd>
+					</dl>
+					<dl class="accordion">
+						<dt class="accordion-title question">Who do I contact with more questions.</dt>
+						<dd class="accordion-content answer">Please email info@frontutah.com with any questions about the conference, including registration, travel, or sponsorships.</dd>
+					</dl>
+					<dl class="accordion">
+						<dt class="accordion-title question">What do I need to do if I am already registered for the 2020 Front UX & Product Management Case Study Conference?</dt>
+						<dd class="accordion-content answer">Nothing. Tickets that have already been purchased will automatically be transitioned to the new schedule.</dd>
+					</dl>
+				</li>
+				<li class="faq-category">
+					<dl class="accordion">
+						<dt class="accordion-title question">What do I need to do if I am already registered for the 2020 Front UX & Product Management Workshop Series?</dt>
+						<dd class="accordion-content answer">If you would like to just attend the workshops, nothing. If you would like to stay Thursday and Friday and also attend the cast study talks, then please reach out to us for a code to purchase tickets for the other half of the conference.</dd>
+					</dl>
+					<dl class="accordion">
+						<dt class="accordion-title question">I have not registered yet. How can I register?</dt>
+						<dd class="accordion-content answer">Register online at frontutah.com/conference/registration.</dd>
+					</dl>
+					<dl class="accordion">
+						<dt class="accordion-title question">If I opt to get a refund, how will I get the refund?</dt>
+						<dd class="accordion-content answer">First, we encourage you to be patient. We understand that the coronavirus situation is scary, but we feel extremely optimistic about us being able to gather in November. If you have already purchased your ticket, then you’ve been able to take advantage of the early bird discount, so we encourage you to hand on to your ticket. That said, if you still want to get a refund, you may do so up to one month before the conference by logging into Eventbrite and following these instructions. As with changes, only the person who purchased the tickets can request a refund.</dd>
+					</dl>
+					<dl class="accordion">
+						<dt class="accordion-title question">What about my hotel room and airline ticket? Will they be rescheduled? Can they be refunded?</dt>
+						<dd class="accordion-content answer">While we arranged for a group discount at a preferred hotel, all bookings were done independently. You will need to reach out to your hotel and airline directly to reschedule. Most are waiving change fees due to the situation with the coronavirus.</dd>
+					</dl>
+				</li>
+			</ul>
+		</section>
+		
+		
+		
+		<section class="join-us">
+			<h2>Join us at the Front!</h2>
+			<p>In it's 6th year, Front is a sell-out event, with  
+				<strong>800 annual attendees from 8 countries and 34 states</strong> 
+				across the US. Join us at the Front to share, learn, and be inspired 
+				to create amazing products.</p>
+			<p><a href="/conference/registration" class="button">Register</a></p>
+		</section>
+	</main>
+
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
+
+?>
