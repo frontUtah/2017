f4ae403073f4ae2c8ece56836ed6f20fa207a9f1
Wade Shearer <me@wadeshearer.com>
2020-09-16 20:57:12 -0600

    Removed LinkedIn and Facebook tracking

--- includes/header.php
+++ includes/header.php
@@ -29,30 +29,6 @@ if(isset($meta['opengraph']) && !empty($meta['opengraph'])) {
 	<link rel="apple-touch-icon" href="/images/apple-touch-icon.png" type="image/png" />
 	<link rel="mask-icon" href="/images/mask-icon.svg" color="gray" />
 	<link rel="shortcut icon" href="/images/favicon.ico" />
-
-	<!-- Facebook Pixel Code -->
-	<script>
-	  !function(f,b,e,v,n,t,s)
-	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-	  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-	  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-	  n.queue=[];t=b.createElement(e);t.async=!0;
-	  t.src=v;s=b.getElementsByTagName(e)[0];
-	  s.parentNode.insertBefore(t,s)}(window, document,'script',
-	  'https://connect.facebook.net/en_US/fbevents.js');
-	  fbq('init', '279063112700042');
-	  fbq('track', 'PageView');
-	  fbq('track', 'CompleteRegistration');
-	  fbq('track', 'AddPaymentInfo');
-	  fbq('track', 'Purchase');
-	  fbq('track', 'Lead');
-	  fbq('track', 'AddToCart');
-	  fbq('track', 'ViewContent');
-	</script>
-	<noscript><img height="1" width="1" style="display:none"
-	  src="https://www.facebook.com/tr?id=279063112700042&ev=PageView&noscript=1"
-	/></noscript>
-	<!-- End Facebook Pixel Code -->
 </head>
 <body>
 
