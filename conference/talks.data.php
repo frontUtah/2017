489beca8139ab6cff5d7fae493eaf0fc69114904
Wade Shearer <me@wadeshearer.com>
2021-05-17 23:28:31 -0600

    Added talks for first 6 speakers

--- conference/talks.data.php
+++ conference/talks.data.php
@@ -8,6 +8,62 @@
 
 
 
+// 
+$talks[] = array(
+	'title' => 'Private by default',
+	'description' => "The future of software products is private, personalized, and on-demand. In this talk, Gabe will cover the lessons learned from the launch of private personalization startup Canopy's first consumer app, Tonic and how they can be applied to making future-proof decisions when designing products in 2020 and beyond.",
+	'day' => 1,
+	'slot' => 3,
+	'speaker' => 'gabrielvaldivia'
+);
+
+// 
+$talks[] = array(
+	'title' => 'Lessons learned from 451 days of cross-functional leadership at Pluralsight',
+	'description' => "For years Pluralsight reaped the benefits of cross-functional teams but relied on a functional leadership org design. Come see what we learned in the last 451 days after moving to an org design where each leader is responsible for product, design, and engineering. While not a silver bullet, the new design has delivered on its promises. Teams no longer rely on multiple leaders—or experience the inevitable confusion that follows. They also enjoy less friction as they work together and collaborate more powerfully across teams. Whether you’re a leader, or a contributor, you’ll take home tactical lessons that will improve how you work with other disciplines.",
+	'day' => 1,
+	'slot' => 6,
+	'speaker' => 'daverackham'
+);
+
+// 
+$talks[] = array(
+	'title' => 'Saying goodbye to a good idea',
+	'description' => "You've done extensive user research, you've investigated competing alternatives, you've crunched the numbers. After completing significant discovery work, you've become very invested in the problem and are excited about possible solutions … But you realize the right decision for the business is to pivot. In this presentation, I'll discuss my experience moving away from a favorite project and what I learned from the process - how to fail faster without the feeling of failure.",
+	'day' => 1,
+	'slot' => 5,
+	'speaker' => 'lauraluttmer'
+);
+
+// 
+$talks[] = array(
+	'title' => 'Scaling decisions as a product team',
+	'description' => "What happens when your team gets really big? In this talk, Elaine tells the story of how the Adobe XD product management team scaled decisions for a recently-released feature that cut across almost the entire product. From processes to values, specific challenges to behavioral analysis, this case study covers how a product team can pull together to make decisions that are coherent to a system at scale.",
+	'day' => 1,
+	'slot' => 1,
+	'speaker' => 'elainechao'
+);
+
+// 
+$talks[] = array(
+	'title' => 'Leading from compulsion to compassion',
+	'description' => "The modern-day workplace is getting to be demanding and we as humans continue to demand more from ourselves. Both emerging and seasoned leaders are continuously faced with choices and decisions to make. How we make these decisions impacts our teams, our work, and, most importantly, has a significant impact on our self. Outside of being a parent, being a leader is perhaps the only other role that comes with its fair share of freedom and responsibility. <br /><br />Who or what inspires you to lead? How aware are you to show up as the leader you want to be? Is there a practice or ritual to bring about an authentic, compelling, yet sustainable way to lead? Given her own upbringing as an Indian-American amidst a culture of tolerance and respect, she thrives in the diversity of thought and action. Drawing from her own personal and professional experiences, Chetana’s talk will explore what it means to lead from within and how the current workplace makes it difficult to do so. How our cultural and personal values intersect with who we are as leaders. What drives someone to lead compulsively? <br /><br />Let’s explore following yourself and to lead from a place of compulsion to compassion.",
+	'day' => 1,
+	'slot' => 2,
+	'speaker' => 'chetanadeorah'
+);
+
+// 
+$talks[] = array(
+	'title' => 'What do you do when no one cares about your product? How we turned Podium’s feedback tool from a failing beta to a successful launch',
+	'description' => "<p>When I joined Podium, our Feedback tool was in a precarious state. As one sales VP explained, “The percentage of customers we could sell this to is in the single digits.” There were discussions of killing the product completely. Six months later, we launched it publicly to all customers, and it went on to sell millions in the following year. Here’s how we got there, and how you can too.</p><p>With most products, there can be many obstacles to product-market fit:</p><ul><li>Wrong product</li><li>Wrong market</li><li>Wrong message</li></ul><p>I’ll cover specific tactics we used to understand the problem in all three of these areas, including:</p><ul><li>An internal listening tour</li><li>JTBD interviews with customers</li><li>Analyzing data</li><li>Exploring the ways customers were hacking the product</li><li>Evaluating against competitor options</li><li>And more...</li></ul><p>We’ll also discuss how we used that research to transform the product and its appeal… and the things we missed that created unexpected results.</p>",
+	'day' => 1,
+	'slot' => 4,
+	'speaker' => 'kaelaworthen'
+);
+
+
+
 // TEMPLATE
 /*
 // 
