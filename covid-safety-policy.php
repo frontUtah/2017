ee08051b9cb615f7e8b69425497b5677b34ae467
Wade <me@wadeshearer.com>
2021-12-07 16:17:33 -0700

    Added covid-safety-policy back

---
+++ covid-safety-policy.php
@@ -0,0 +1,32 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/init.php');
+$title = 'COVID Safety Policy - Front';
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
+?>
+	<main class="article-page serious">
+		<section class="hero">
+			<h1>COVID Safety Policy</h1>
+		</section>
+		<section class="body">
+			<h2>COVID Safety Policy</h2>
+			<p>The safety and well-being of our attendees, speakers, instructors, sponsors, and staff is our top priority. These unprecedented times call for unprecedented levels of compassion, charity, and wisdom, with each of us doing our part to limit the spread of this virus. As a business that brings large groups of people together, we take this responsibility seriously. We offer you our commitment that we will only host events that we feel can be done safely. We are continuously monitoring developments of the COVID-19 pandemic, reviewing guidelines from the CDC, and collaborating with local government agencies to implement necessary protocols.</p>
+			<p>While the following preventative measures are put into place to reduce the spread of the COVID-19 virus, we, under no circumstances, are able to guarantee that you or a guest will not become infected with the virus while traveling or attending the event.</p>
+			<p>Adherence to the following event policy and guidelines will be continuously monitored and enforced by Front staff. Please review the agreement below. Additionally, your signature will be required as a confirmation of your intent and commitment to abide by policies put in place to protect attendees, speakers, instructors, sponsors, and staff.</p>
+ </p>
+ 			<ul>
+ 				<li>Attendees, sponsors, speakers, and staff experiencing or exhibiting any COVID-19 related symptoms as outlined by the federal and state Center for Disease Control and Prevention (CDC) such as fever, dry cough, or shortness of breath will not be allowed to enter the event. Attendees that begin experiencing related symptoms agree to notify staff and leave immediately.</li>
+ 				<li>Anyone who has traveled internationally within the past 14 days may not attend, present, or work at the event.</li>
+ 				<li>Anyone that believes they have been recently exposed to a person or animal with a positive and confirmed case if COVID-19 may not attend, present, or work at the event.</li>
+ 				<!--
+ 				<li>All attendees, sponsors, speakers, and staff will be required to practice social distancing.</li>
+ 				<li>All attendees, sponsors, speakers, and staff will be required to wear masks over their nose and mouth.</li>
+				<li>All attendees, sponsors, speakers, and staff will be required to provide proof of vaccination or a negative COVID-19 test result with a date and time stamp within 48 hours of the event.</li>
+				<li>All attendees, sponsors, speakers, and staff will be required to sign a <a href="https://docs.google.com/document/d/12hlA-lkq-ztQZ3j_R2xgMhdVCCNKxXh8EuVukQNGY5E/edit">waiver and release of liability form</a>. You may print it beforehand and bring it with you or sign one at the event during registration.</li>
+				<li>Attendees, sponsors, speakers, and staff that exhibit any behavior in violation to the Front safety policy will be removed from the event by Front staff.</li>
+				-->
+			</ul>
+		</section>
+	</main>
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
+?>
