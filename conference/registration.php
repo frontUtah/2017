9c992f75bf64719baa7166c34715414c9ca77f6c
Wade <me@wadeshearer.com>
2022-02-01 14:23:20 -0700

    Update 2022 conference early bird deadline

--- conference/registration.php
+++ conference/registration.php
@@ -41,11 +41,11 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 					<dt><div></div></dt>
 					<dd class="early">
 						Early bird pricing
-						<small>30% off through 4 February 2022</small>
+						<small>30% off through 4 March 2022</small>
 					</dd>
 					<dd class="standard">
 						Standard pricing
-						<small>Starting 5 February 2022</small>
+						<small>Starting 5 March 2022</small>
 					</dd>
 				</dl>
 				<dl class="full">
