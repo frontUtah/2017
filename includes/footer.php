c498a0817a7e337a31a979d411b3924fd6872de1
Wade <me@wadeshearer.com>
2022-02-01 09:52:34 -0700

    Improved styling of mailing list subscription form in footer

--- includes/footer.php
+++ includes/footer.php
@@ -19,11 +19,7 @@
 
 		<!-- Begin Mailchimp Signup Form -->
 		<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-		<style type="text/css">
-			#mc_embed_signup{clear:left; margin-bottom: 50px; width:100%;}
-			/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-			   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-		</style>
+
 		<div id="mc_embed_signup">
 		<form action="https://frontutah.us2.list-manage.com/subscribe/post?u=5a9bade195be7c98130573919&amp;id=e8d567ca7f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 		    <div id="mc_embed_signup_scroll">
