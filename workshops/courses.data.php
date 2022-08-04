85208cb4178bb41f71e6e9daa4a1b5fc15c01ebe
Wade <me@wadeshearer.com>
2022-08-04 09:29:23 -0600

    Added old workshop instructors and courses for Ben to recruit instructors

--- workshops/courses.data.php
+++ workshops/courses.data.php
@@ -8,6 +8,77 @@
 
 $courses[] = array();
 
+// Drew Bridewell
+$courses[] = array(
+	'title' => 'The building, scaling, and evolution of a Design System',
+ 	'description' => "Product design over the last years has taken a substantial leap forward in progress. We now have multiple tools to choose from for executing our solutions, along with a collection of new responsibilities that each one of us needs to adapt to stay current.</br></br>In this Workshop, join Drew Bridewell, Senior Design Specialist from InVision, as he shares a wave of packaged up fresh design system content that you’ve never seen before.<br /><br />He’ll walk you through how you can elevate your thinking around design systems, as well as the practice that goes into successfully implementing one. </br></br>During the workshop Drew will provide take-home worksheets that can help you throughout the creation and governance process of building systems. He’ll finish by providing you the best-in-class habits to nurture your systems over time.",
+ 	'track' => 'ux',
+ 	'day' => 1,
+ 	'slot' => 1,
+ 	'instructor' => 'drewbridewell',
+	'level' => 'advanced'
+);
+
+// Mariah Hay
+ $courses[] = array(
+ 	'title' => 'Product Leadership Fundamentals: Find, Develop, and Keep Your Team Healthy',
+ 	'description' => "You’re an experienced product practitioner who has a love of mentoring and an ambition for leadership but are not sure where to start. To make that shift you need to arm yourself with some tactical skills around building a team, developing a team, and even having the hard conversations needed to keep the team healthy. In this workshop, we will be breaking our time into focusing on these three areas, reviewing tactics and tools, and learning through doing. <br><br>Experience Team Building 101: How to recruit, interview and identify the best person for your team. We will look at strategies for finding amazing applicants, effective ways of interviewing, and things to looks for and consider in selecting a new team member.<br><br>Developing Your Team: Tools to develop your PMs and UX Designers, and create career paths within your organization We will look at defining what career paths exist / should exist in your company, learn to use tools that help us guide a conversation to understand the many dimensions of your team member, and practice ways to select and prioritize actionable things you can work with your team member to improve.<br><br>Conversations: How to manage Product Managers and UX Designers who are dropping the ball. We will talk about one of the hardest and most important parts of leadership, managing team members who are dropping the ball. This is easier said than done. First, we need to understand why the ball is being dropped (system, workload, communication, soft skills, disengagement). If it is a system or workload issue, how do we co-author a solution with the team? If it is a communication or soft skills issue, how do we coach the individual? And if it is a disengagement issue, how do we have the hard conversation with our team member that they must step up or move on." ,
+ 	'track' => 'product-management',
+ 	'day' => 1,
+ 	'slot' => 2,
+ 	'instructor' => 'mariahhay',
+ 	'level' => 'intermediate'
+ );
+
+// Garth Braithwaite
+ $courses[] = array(
+ 	'title' => 'How to Establish and Maintain a Design System',
+ 	'description' => "As teams and products grow it can become complicated to maintain consistency across all properties, additionally, there is often a duplication of initial work for each product starting from scratch. By establishing and maintaining a company-wide design language and design system implementation, teams are better equipped to ensure consistency, reduce duplication of work, and improve overall design and code quality.<br><br.In this workshop, you will get hands-on experience with initial ideation and planning of a design system, as well as designing initial tokens, components, assets, and usage guidelines. Along the way, we'll discuss common pitfalls and pain points associated with organizing a design system and establishing it throughout a company.",
+ 	'track' => 'ux',
+ 	'day' => 1,
+ 	'slot' => 3,
+ 	'instructor' => 'garthdbraithwaite',
+ 	'level' => 'basic',
+ 	'gumroad_product_id' => 'front-workshops-instructor-garthdbraithwaite',
+ 	'room' => 1
+ );
+
+ // Sophia Prater
+ $courses[] = array(
+ 	'title' => 'Tackle complexity with object-oriented UX',
+ 	'description' => "The best time to get super-curious about all the unknown unknowns is early on in a project. But all too often, we end up brushing complex business rules and information architecture aside in the name of “low fidelity.” Unfortunately, as we avoid asking hard questions to our users, stakeholders, and developers — while designing features and screens without full context  — we end up with piles of rework. In this workshop, you’ll learn about the four tough questions you should be asking early on—to make the rest of the project easier and more successful.<ul><li>What are the important objects?</li><li>What are their relationships to each other? </li><li>What do users want to do to these objects?</li><li>What is the structure of each object?</li></ul>And then, you’ll learn how to answer these four questions through collaborative, structured, and super-effective exercises. <br /><br />You’ll come out of the workshop with four new facilitation and design frameworks that will help you and your team tackle complex problems with confidence.",
+ 	'track' => 'ux',
+ 	'day' => 1,
+ 	'slot' => 4,
+ 	'instructor' => 'sophiaprater',
+ 	'level' => 'basic',
+ 	'room' => 2
+ );
+
+ // Vicky Thomas
+ $courses[] = array(
+ 	'title' => 'Creating product vision from scratch',
+ 	'description' => "So you need a product vision... now what? This workshop is all about creating product vision and strategy from the ground up. We'll talk about the different components of an effective product vision and strategy, and how to create them. You'll leave the workshop with a gameplan to create these things for your team in 2 weeks or less.",
+ 	'track' => 'ux',
+ 	'day' => 1,
+ 	'slot' => 5,
+ 	'instructor' => 'vickythomas',
+ 	'level' => 'basic',
+ 	'room' => 3
+ );
+
+ // Jaekob Chenina
+ $courses[] = array(
+ 	'title' => 'How to create intelligent products',
+ 	'description' => "According to Gartner “AI Technologies will be in almost every new software product by 2020”. With this explosion of AI products, many will fail. However, there are common trends that are emerging among the intelligent products that are succeeding. So what is it, that makes an AI product that customers love? In this workshop, you will deep dive on the most relevant concepts across various disciplines that help answer this question.<br /><br /><ul><li>You will learn:</li><li>Analytics for AI products</li><li>Machine Learning for AI products</li><li>UX for AI Products</li><li>Common Use Cases for AI Products</li><li>Discovering AI Product Opportunities</li></ul><br /><br />You will also learn how to embed AI into your software development methodology and design an effective AI product team. Through exercises, case-studies and lessons learned, this workshop will empower your product team with an actionable path to incorporate AI in an impactful way into your business.",
+ 	'track' => 'ux',
+ 	'day' => 1,
+ 	'slot' => 6,
+ 	'instructor' => 'jaekobchenina',
+ 	'level' => 'intermediate',
+ 	'room' => 4
+ );
+
 
 /*
 
--- workshops/instructors.data.php
+++ workshops/instructors.data.php
@@ -4,7 +4,95 @@
  * Use '</br></br>' to separate paragraphs
  */
 
+// Garth Braithwaite
+ $instructors[] = array(
+ 	'slug' => 'garthdbraithwaite',
+ 	'first' => 'Garth',
+ 	'last' => 'Braithwaite',
+ 	'title' => 'Senior Computer Scientist',
+ 	'company' => 'Adobe',
+ 	'discipline' => 'ux',
+ 	'photo' => 'frontpc18/instructor_garthbraithwaite.jpg',
+ 	'bio' => "Front End Developer and User Experience Designer at Adobe working on developing and maintaining Adobe's internal design system.",
+ 	'twitter' => 'GarthDB',
+ 	'instagram' => '',
+ 	'linkedin' => 'https://www.linkedin.com/in/garthdb/'
+ );
 
+ // Drew Bridewell
+ $instructors[] = array(
+ 	'slug' => 'drewbridewell',
+	'first' => 'Drew',
+	'last' => 'Bridewell',
+	'title' => 'Design Specialist',
+	'company' => 'InVision',
+	'discipline' => 'ux',
+	'photo' => 'frontworkshops18/speaker_drewbridewell.jpg',
+	'bio' => 'Drew collaborates, consults, and shares insights with companies across the world who are looking for best practices around tools, collaboration, and building design systems.</br></br>His goal is to empower designers, teams, and organizations across the world to build the worlds best digital experiences!</br></br>Drew also focuses on the people, the practice of User Experience Design, and the InVision platform.</br></br>Previously Drew was a design manager and lead designer at LinkedIn, where he focused on Learning Solutions like Lynda.com and LinkedIn Learning.</br></br>He also teaches UX on Lynda.com and LinkedIn Learning through his weekly series Practical UX Weekly.',
+	'twitter' => 'abridewell',
+	'instagram' => 'abridewell/',
+	'linkedin' => 'https://www.linkedin.com/in/abridewell/'
+);
+
+ // Mariah Hay
+ $instructors[] = array(
+ 	'slug' => 'mariahhay',
+ 	'first' => 'Mariah',
+ 	'last' => 'Hay',
+ 	'title' => 'VP, Product',
+ 	'company' => 'Pluralsight',
+ 	'discipline' => 'product-management',
+ 	'photo' => 'frontpc18/instructor_mariahhay.jpg',
+ 	'bio' => 'Mariah Hay is the VP of User Experience for Pluralsight, an online subscription based education provider for the technology community. As a thought leader in human-centered design and user experience strategy, Mariah enjoys working with teams whose goal is to craft excellence for the end user. Her experience with industrial and interaction design teams serving high-level stakeholders reflects her personal goals of directing business through design excellence from the ground up. Her expertise in implementing human-centered design product practices at the enterprise level stems from over a decade of tenure in the industry including serving as Professor of Design Management at Savannah College of Art and Design, Directing UX for Universal Mind, and serving as Director of User Experience AMC Health. Mariah holds an MFA in Industrial Design from Savannah College of Art and Design.',
+ 	'twitter' => 'mariahhay',
+ 	'instagram' => '',
+ 	'linkedin' => 'https://www.linkedin.com/in/mariahhay/'
+ );
+
+ // Vicky Thomas
+$instructors[] = array(
+	'slug' => 'vickythomas',
+	'first' => 'Vicky',
+	'last' => 'Thomas',
+	'title' => 'Director Product Management',
+	'company' => 'Vivint',
+	'discipline' => 'product-management',
+	'photo' => 'frontworkshops18/speaker_vickythomas.jpg',
+	'bio' => 'Vicky Thomas is the Director of Product Management for Vivint Smart Home’s mobile apps. At Vivint, Vicky leads product teams to envision and build the next generation of smart home experiences, leveraging software, hardware, AI, voice, and conversational interfaces. Prior to joining Vivint, Vicky served as Lead Product Manager at Lucid Software, makers of top-ranked productivity apps Lucidchart and Lucidpress, and as a Product Manager at Adobe on Adobe Analytics. She is passionate about building world-class product teams and bringing new ideas to life.</br></br>Vicky holds an S.B. in Aerospace Engineering from MIT, with a certificate in Advanced Engineering Leadership from the Gordon-MIT Engineering Leadership Program. She has received numerous awards for her industry and community impact, including a 2015 Women Tech Award from the Women Tech Council, and a spot on Utah Business Magazines 2017 30 Women to Watch list. In her free time, Vicky enjoys backpacking with her husband and dog, playing soccer, backcountry skiing, and flying single-engine aircraft.',
+	'twitter' => '__vickyt',
+	'instagram' => '',
+	'linkedin' => 'https://www.linkedin.com/in/vicky-thomas-2b6a259/'
+);
+
+// Jaekob Chenina
+$instructors[] = array(
+	'slug' => 'jaekobchenina',
+	'first' => 'Jaekob',
+	'last' => 'Chenina',
+	'title' => 'Product Manager',
+	'company' => 'Adobe',
+	'discipline' => 'product-management',
+	'photo' => 'frontworkshops18/speaker_jaekobchenina.jpg',
+	'bio' => "Jaekob is leading AI innovation at Adobe Analytics, the heart of Adobe’s next-gen digital experience cloud. Over the last five years Jaekob has worked in startup, non-profit, and enterprise environments. In each case, Jaekob sought out and led projects that were disrupting the status quo. His passion for trailblazing opportunities married with his background in AI led him to focus his career on building Intelligent Products. When not at work, Jaekob enjoys spending time with his Russian/Ukrainian wife Galina and their three children. He and his family have a passion for all forms of exploration from ice-fishing to genealogy.",
+	'twitter' => 'chenina10',
+	'instagram' => '',
+	'linkedin' => 'https://www.linkedin.com/in/jchenina/'
+);
+
+// Sophia Prater
+ $instructors[] = array(
+ 	'slug' => 'sophiaprater',
+ 	'first' => 'Sophia',
+ 	'last' => 'Prater',
+ 	'title' => 'Senior UX Designer',
+ 	'company' => 'Nine Labs',
+ 	'discipline' => 'ux',
+ 	'photo' => 'headshots/sophiaprater.jpg',
+ 	'bio' => "Sophia Prater is founder and lead UX designer at Rewired and the chief evangelist of object-oriented UX. Sophia teaches her OOUX methodologies at conferences, within companies, and through her OOUX Certification Program. She is the host of the OOUX Happy Hour meetup and the OOUX Podcast.<br /><br />Sophia has brought the complexity-untangling magic of OOUX to companies such as Facebook, Mastercard, Macy’s, Credit Karma, Hubspot, Intercom, Delta Airlines, CNN, and many more.<br /><br />Sophia lives outside of Atlanta in the beautiful north Georgia mountains, the wine country of the southeast United States, with her husband and business partner, Luke.",
+ 	'twitter' => '',
+ 	'instagram' => '',
+ 	'linkedin' => 'https://www.linkedin.com/in/sophiav/'
+ );
 
 /*
 
