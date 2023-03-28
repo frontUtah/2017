5aaec2d2090a291e7515431934834f2cb7b9c5a6
Ben Peck <ben@frontutah.com>
2023-03-28 09:30:13 -0600

    Added new sponsors

--- conference/sponsors.data.php
+++ conference/sponsors.data.php
@@ -4,7 +4,41 @@
  * Use '</br></br>' to separate paragraphs
  */
 
-/* Underbelly
+/* 
+
+// Sendbird
+$sponsors[] = array(
+	'name' => 'Sendbird',
+	'slug' => 'sendbird',
+	'tier' => 2,
+	'url' => 'https://sendbird.com',
+	'logo' => 'sendbird.svg',
+	'message' => "Sendbird believes conversations are at the heart of building relationships and getting things done. As such, we built the world's most proven conversations platform for mobile apps across chat, voice, and video. Industry leaders like Doordash, Reddit, and Hinge use Sendbird to drive increased transactions & loyalty for hundreds of millions of users every month. Headquartered in San Mateo, California, Sendbird is backed by re ICONIQ Growth, STEADFAST Capital Ventures, Tiger Global Management, Meritech Capital, Emergence Capital, Shasta Ventures, August Capital, Funders Club, World Innovation Lab, and Y Combinator."
+);
+
+// Coding Space
+$sponsors[] = array(
+	'name' => 'Coding Space',
+	'slug' => 'codingspace',
+	'tier' => 2,
+	'url' => '',
+	'logo' => '',
+	'message' => ""
+);
+
+*/
+
+// Commandbar
+$sponsors[] = array(
+	'name' => 'Commandbar',
+	'slug' => 'commandbar',
+	'tier' => 1,
+	'url' => 'https://www.commandbar.com',
+	'logo' => 'commandbar.svg',
+	'message' => ""
+);
+
+// Underbelly
 $sponsors[] = array(
 	'name' => 'Underbelly',
 	'slug' => 'underbelly',
@@ -12,6 +46,16 @@ $sponsors[] = array(
 	'url' => 'https://underbelly.is',
 	'logo' => 'underbelly.svg',
 	'message' => "Underbelly is an expert design, development, and production partner. Businesses come to us when they are blocked, stretched thin, and need an expert perspective to move their vision to market. We actualize digital products and experiences through strategy, design, and technology. Through collaboration and craftsmanship, we make things better together. Learn more about how our team of designers, developers, and filmmakers can bring your ideas to life <a href=\"https://underbelly.is\">here</a>."
+);
+
+// Lucid
+$sponsors[] = array(
+	'name' => 'Lucid',
+	'slug' => 'lucid',
+	'tier' => 2,
+	'url' => 'https://lucid.co',
+	'logo' => 'lucid.svg',
+	'message' => "Lucid started in Utah 10 years ago and now serves 30+ million users and 99% of the Fortune 500. Lucid's visual collaboration suite gives teams the power to go from imagining the future to building it. Their team builds Lucidspark, a virtual whiteboard where teams can put their heads together to inspire innovations and bring them to reality, and Lucidchart, the intelligent diagramming application that empowers teams to clariify complexity, align their insights, and build the future–faster."
 );
 
 // Dovetail
@@ -24,17 +68,6 @@ $sponsors[] = array(
 	'message' => "Dovetail's mission is to enable the world to create better products and services through deep customer understanding. We empower 45,000+ users, from agencies to universities to Fortune 100 companies, to make sense of all their customer research data in one intuitive, collaborative, and powerful research platform. Find out more about us <a href=\"https://dvtl.link/3IS98Re\">here</a>."
 );
 
-// Sendbird
-$sponsors[] = array(
-	'name' => 'Sendbird',
-	'slug' => 'sendbird',
-	'tier' => 2,
-	'url' => 'https://sendbird.com',
-	'logo' => 'sendbird.svg',
-	'message' => "Sendbird believes conversations are at the heart of building relationships and getting things done. As such, we built the world's most proven conversations platform for mobile apps across chat, voice, and video. Industry leaders like Doordash, Reddit, and Hinge use Sendbird to drive increased transactions & loyalty for hundreds of millions of users every month. Headquartered in San Mateo, California, Sendbird is backed by re ICONIQ Growth, STEADFAST Capital Ventures, Tiger Global Management, Meritech Capital, Emergence Capital, Shasta Ventures, August Capital, Funders Club, World Innovation Lab, and Y Combinator."
-);
-*/
-
 // Volley
 $sponsors[] = array(
 	'name' => 'Volley',
@@ -44,6 +77,7 @@ $sponsors[] = array(
 	'logo' => 'volley.svg',
 	'message' => "The  fastest ⚡️ way to monetize your expertise ✨<br><br>Create revenue-generating experiences powered by your expertise in minutes. No cost to set up. Only pay a small fee when you make a sale. <br><br>Used by 40k+ of the top creators, coaches, and consultants to monetize life changing experiences."
 );
+
 
 // TEMPLATE
 /*
